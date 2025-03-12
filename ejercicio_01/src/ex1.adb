f41abcd0bd82a7e091efc43c4f7f819d5b0368d7
grochochotowski <g.m.rochowski@gmail.com>
2025-03-13 00:12:30 +0100

    Initialize variables

--- ejercicio_01/src/ex1.adb
+++ ejercicio_01/src/ex1.adb
@@ -1,6 +1,16 @@
-procedure Main is
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Ex1 is
 
 begin
-   --  Insert code here.
+   -- Define data types
+   subtype A is Integer range -40 .. 160;
+   subtype B is Integer range 0 .. 120;
+   subtype C is Integer range 0 .. 225;
+
+   -- Initialize variables
+   A : A := -20;
+   B : B := 30;
+   C : C := 50;
    null;
 end Main;
