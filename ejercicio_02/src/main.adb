4a9247a0d1de4e67551c3dd792060f42e19de883
grochochotowski <g.m.rochowski@gmail.com>
2025-03-13 15:05:24 +0100

    Create loop for checking the string chagractewrs

--- ejercicio_02/src/main.adb
+++ ejercicio_02/src/main.adb
@@ -6,7 +6,18 @@ procedure Main is
    Var : String := "QWERTYU";
 
    begin
-
+      -- Loop for checking string
+      for I in Var'range loop
+         if Var(I) = 'Q' or Var(I) = 'W' then
+            Put_Line("Option 1");
+         else if Var(I) = 'E' or Var(R) = 'W' or Var(I) = 'T' then
+            Put_Line("Option 2");
+         elsif Var(I) = 'Y' then
+            Put_Line("Option 3");
+         else
+            Put_Line("Another option");
+         end if;
+      end loop;
 
    null;
 end Main;
