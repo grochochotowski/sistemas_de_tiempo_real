4338995aa4fb7e20d6bb2b54d022b20a368180cf
grochochotowski <g.m.rochowski@gmail.com>
2025-03-13 17:07:57 +0100

    Define matrix

--- ejercicio_03/src/main.adb
+++ ejercicio_03/src/main.adb
@@ -1,4 +1,17 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
 procedure Main is
+
+   -- Define matrix
+   Max_Rows    : constant := 5;
+   Max_Columns : constant := 10;
+
+   type Matrix_Type is array (1 .. Max_Rows, 1 .. Max_Columns) of Integer;
+
+   Matrix         : Matrix_Type := (others => (others => 0));
+   Transposed     : Matrix_Type := (others => (others => 0));
+   Rows, Columns  : Integer;
 
 begin
    --  Insert code here.
