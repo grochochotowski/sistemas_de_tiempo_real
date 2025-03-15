a56dddeef9f51389fe5d6db2aff9f5e226834741
grochochotowski <g.m.rochowski@gmail.com>
2025-03-15 23:11:42 +0100

    Implement queue

--- ejercicio_04/src/main.adb
+++ ejercicio_04/src/main.adb
@@ -1,4 +1,11 @@
+with Ada.Text_IO; use Ada.Text_IO
+with Ada.Containers.Queues; use Ada.Containers.Queues;
+with Ada.Containers.Vectors; use Ada.Containers.Vectors;
+
 procedure Main is
+   -- Queue implementation
+   package Queue_Type is new Queue (Index_Type => Positive, Element_Type => Integer);
+   Q : Queue_Type;
 
 begin
    --  Insert code here.
