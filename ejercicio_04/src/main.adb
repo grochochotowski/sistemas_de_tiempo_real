c1452471f95a4416a23a13a3964826f3d58d8a1d
grochochotowski <g.m.rochowski@gmail.com>
2025-03-16 23:13:15 +0100

    Log to the console operation of appending numbers to queue

--- ejercicio_04/src/main.adb
+++ ejercicio_04/src/main.adb
@@ -10,14 +10,21 @@ begin
    -- Initialize the queue
    Integer_Vector.Clear(Queue);
 
+   New_Line;
+
    -- Add 10 elements to the queue (FIFO - First In, First Out)
    for I in 1 .. 10 loop
+      Put_Line("Appending to queue: " & Integer'Image(I));
       Integer_Vector.Append(Queue, I);
    end loop;
+
+   New_Line;
 
    -- Display the elements in the queue (FIFO)
    Put_Line("Queue elements (FIFO):");
    for Element of Queue loop
       Put_Line(Integer'Image(Element));
    end loop;
+
+   New_Line;
 end Main;
