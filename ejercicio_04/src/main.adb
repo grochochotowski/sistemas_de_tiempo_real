bb30be66b1ea422c98a8c7d35626aa47deb64fc7
grochochotowski <g.m.rochowski@gmail.com>
2025-03-16 23:18:35 +0100

    Move queue implementation to seperate procedure

--- ejercicio_04/src/main.adb
+++ ejercicio_04/src/main.adb
@@ -6,25 +6,30 @@ procedure Main is
    package Integer_Vector is new Ada.Containers.Vectors (Index_Type => Positive, Element_Type => Integer);
    Queue : Integer_Vector.Vector;
 
+   procedure Queue_Implementation is
+   begin
+      -- Initialize the queue
+      Integer_Vector.Clear(Queue);
+      New_Line;
+
+      -- Add 10 elements to the queue (FIFO - First In, First Out)
+      for I in 1 .. 10 loop
+         Put_Line("Appending to queue: " & Integer'Image(I));
+         Integer_Vector.Append(Queue, I);
+      end loop;
+      New_Line;
+
+      -- Display the elements in the queue (FIFO)
+      Put_Line("Queue elements (FIFO):");
+      for Element of Queue loop
+         Put(Integer'Image(Element) & ",");
+      end loop;
+   end Queue_Implementation;
+
+
 begin
-   -- Initialize the queue
-   Integer_Vector.Clear(Queue);
-
+   Put_Line("========== Queue iplementation ==========");
+   Queue_Implementation;
    New_Line;
-
-   -- Add 10 elements to the queue (FIFO - First In, First Out)
-   for I in 1 .. 10 loop
-      Put_Line("Appending to queue: " & Integer'Image(I));
-      Integer_Vector.Append(Queue, I);
-   end loop;
-
-   New_Line;
-
-   -- Display the elements in the queue (FIFO)
-   Put_Line("Queue elements (FIFO):");
-   for Element of Queue loop
-      Put_Line(Integer'Image(Element));
-   end loop;
-
    New_Line;
 end Main;
