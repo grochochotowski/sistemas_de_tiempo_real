bf83ff28a2eefaacb7294f00fd0eeba165c606a8
grochochotowski <g.m.rochowski@gmail.com>
2025-03-23 17:08:26 +0100

    Check if line is not empty or the file is not ending and then proceed

--- practica_01/src/read_data.adb
+++ practica_01/src/read_data.adb
@@ -1,12 +1,20 @@
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Ada.Float_Text_IO;   use Ada.Float_Text_IO;
-with Ada.Text_IO;         use Ada.Text_IO;
-with All_Types;           use All_Types;
+with Ada.Float_Text_IO; use Ada.Float_Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
+with All_Types; use All_Types;
 
 package body Read_Data is
    procedure Read_Row(File : in out File_Type; Data : out Sensor_Data) is
       Dummy_Index : Integer;
    begin
+      -- Skip empty lines and finish program if needed:
+      while End_Of_Line(File) loop
+         Skip_Line(File);
+         if End_Of_File(File) then
+            return;
+         end if;
+      end loop;
+
       Get(File, Dummy_Index);
 
       Get(File, Data.SR1);
