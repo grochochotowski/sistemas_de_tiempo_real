2d332beef70fb9c439ab573daedf6e3fc2a4c8a1
grochochotowski <g.m.rochowski@gmail.com>
2025-03-23 21:13:52 +0100

    Create functionality for multiplication\ of 2 complex numbers

--- ejercicio_05/src/complex_numbers.adb
+++ ejercicio_05/src/complex_numbers.adb
@@ -6,4 +6,17 @@ package body Complex_Numbers is
       return (Re => Left.Re + Right.Re, Im => Left.Im + Right.Im);
    end "+";
 
+   -- Subtraction: (a + bi) - (c + di) = (a-c) + (b-d)i
+   function "-" (Left, Right : Complex) return Complex is
+   begin
+      return (Re => Left.Re - Right.Re, Im => Left.Im - Right.Im);
+   end "-";
+
+   -- Multiplication: (a + bi) * (c + di) = (ac - bd) + (ad + bc)i
+   function "*" (Left, Right : Complex) return Complex is
+   begin
+      return (Re => Left.Re * Right.Re - Left.Im * Right.Im,
+              Im => Left.Re * Right.Im + Left.Im * Right.Re);
+   end "*";
+
 end Complex_Numbers;
