df24be734141ac893c94067c2d19cf9d4114ce2b
grochochotowski <g.m.rochowski@gmail.com>
2025-03-23 15:27:35 +0100

    Change public types from array to records

--- practica_01/src/all_types.ads
+++ practica_01/src/all_types.ads
@@ -1,3 +1,15 @@
 package All_Types is
-   type Row_Table is array (0 .. 9) of Float;
+
+   type Control_State is record
+      ST1 : Float;
+      ST2 : Float;
+      SD1 : Float;
+   end record;
+
+   type Sensor_Data is record
+      SR1 : Float;  -- Radiation sensor [W/m²]
+      ST4 : Float;  -- Ambient temperature [°C]
+      ST3 : Float;  -- Seawater temperature [°C]
+   end record;
+
 end All_Types;
