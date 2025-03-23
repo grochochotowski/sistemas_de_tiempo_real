db598f03115ef9a2a00b01e4ba2834689189ef15
grochochotowski <g.m.rochowski@gmail.com>
2025-03-23 21:31:27 +0100

    Creare procedure for displaying numbers

--- ejercicio_05/src/main.adb
+++ ejercicio_05/src/main.adb
@@ -4,6 +4,15 @@ with Complex_Numbers; use Complex_Numbers;
 
 procedure Main is
 
+   procedure Print_Complex(Label : String; C : Complex) is
+   begin
+      Put(Label & " = (");
+      Put(C.Re, Fore => 0, Aft => 2);
+      Put(", ");
+      Put(C.Im, Fore => 0, Aft => 2);
+      Put_Line(")");
+   end Print_Complex;
+
    -- Define complex numbers
    A, B, Sum, Diff, Prod, Quot, Conj : Complex;
 
@@ -11,6 +20,13 @@ begin
    -- Initialize complex numbers
    A := (Re => 3.0, Im => 2.0);
    B := (Re => 1.0, Im => -4.0);
+
+   -- Do operations
+   Sum  := A + B;
+   Diff := A - B;
+   Prod := A * B;
+   Quot := A / B;
+   Conj := Conjugate(A);
 
    null;
 end Main;
