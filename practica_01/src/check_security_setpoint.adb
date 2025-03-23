83d77a3fcb786e1eb075ce6972ea74c9ca3d583e
grochochotowski <g.m.rochowski@gmail.com>
2025-03-23 17:46:25 +0100

    Add comments

--- practica_01/src/check_security_setpoint.adb
+++ practica_01/src/check_security_setpoint.adb
@@ -7,15 +7,18 @@ package body Check_Security_Setpoint is
    procedure Check_Security(State : in Control_State; Iteration : in Integer) is
       Alarm_File : File_Type;
    begin
+      -- Check temperature
       if State.ST2 > 95.0 then
-
+         -- Show error on screen
          Put_Line("ALARM: ST2 exceeds 95°C at iteration " & Integer'Image(Iteration));
 
+         -- Create alarm_log.txt if it doesn't exist
          if not Exists("src/alarm_log.txt") then
             Create(Alarm_File, Out_File, "src/alarm_log.txt");
             Close(Alarm_File);
          end if;
 
+         -- Log error to error_log.txt
          Open(Alarm_File, Append_File, "src/alarm_log.txt");
          Put_Line(Alarm_File, "ALARM: ST2 exceeds 95°C at iteration " & Integer'Image(Iteration));
          Close(Alarm_File);
