f4f18dc350e08f5bf666b1f2184f5dd51c1a9ca1
grochochotowski <g.m.rochowski@gmail.com>
2025-03-23 21:27:11 +0100

    Define and initizalize sample numbers

--- ejercicio_05/src/main.adb
+++ ejercicio_05/src/main.adb
@@ -1,6 +1,16 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Float_Text_IO; use Ada.Float_Text_IO;
+with Complex_Numbers; use Complex_Numbers;
+
 procedure Main is
 
+   -- Define complex numbers
+   A, B, Sum, Diff, Prod, Quot, Conj : Complex;
+
 begin
-   --  Insert code here.
+   -- Initialize complex numbers
+   A := (Re => 3.0, Im => 2.0);
+   B := (Re => 1.0, Im => -4.0);
+
    null;
 end Main;
