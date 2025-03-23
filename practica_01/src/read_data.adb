4b323b14944456c37c0dc2758c0258ddbaf2e40f
grochochotowski <g.m.rochowski@gmail.com>
2025-03-23 15:18:41 +0100

    Update read data to read 1 line at a time

--- practica_01/src/read_data.adb
+++ practica_01/src/read_data.adb
@@ -1,31 +1,22 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Float_Text_IO; use Ada.Float_Text_IO;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Read_Data;
+with All_Types; use All_Types;
 
 package body Read_Data is
     procedure Read_Text (File_Name :  in String; Data : out Array_Data) is
       File : File_Type;
-      Dummy_Index : Integer;
-      Value1, Value2, Value3 : Float;
-      Index : Integer := 1;
+      Value : Float;
+      Index : Integer := Data'First;
 
    begin
       Open (File, In_File, File_Name);
 
       while not End_Of_File(File) and then Index <= Data'Last loop
-         Get (File, Dummy_Index);
-         Get (File, Value1);
-         Get (File, Value2);
-         Get (File, Value3);
-
-         Data(Index)(1) := Value1;
-         Data(Index)(2) := Value2;
-         Data(Index)(3) := Value3;
-
+         Get(File, Value);
+         Data(Index) := Value;
          Index := Index + 1;
       end loop;
+      Close(File);
 
-      Close (File);
    end Read_Text;
 end Read_Data;
--- practica_01/src/read_data.ads
+++ practica_01/src/read_data.ads
@@ -1,6 +1,5 @@
-package Read_Data is
-   type Row_Type is array (1 .. 3) of Float;
-   type Array_Data is array (1 .. 20) of Row_Type;
+with All_Types; use All_Types;
 
-   procedure Read_Text (File_Name:  in String; Data : out Array_Data);
-end Read_Data;
+package Read_Text_Module is
+   procedure Read_Text (File_Name:  in String; Data : out Row_Table);
+end Read_Text_Module;
