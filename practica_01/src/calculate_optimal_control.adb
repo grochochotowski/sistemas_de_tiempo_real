4cbeba3ec7872a18d0e727d319fde2ab61a8c8f6
grochochotowski <g.m.rochowski@gmail.com>
2025-03-23 19:30:22 +0100

    calculate optimal values

--- practica_01/src/calculate_optimal_control.adb
+++ practica_01/src/calculate_optimal_control.adb
@@ -24,5 +24,51 @@ package body Calculate_Optimal_Control is
       SC2_opt : Float;
       
    begin
+      -- Equation 2
+      Tt := (Prev_State.ST1 + Prev_State.ST2) / 2.0;
       
+      -- Calculate optimal SC1
+      if ST2_opt - Prev_State.ST1 = 0.0 then
+         SC1_opt := 16.0;  -- fallback default
+      else
+         SC1_opt := (c / (Cp * Rho)) * (Beta * Leq * Sensor.SR1 - H * (Tt - Sensor.ST4)) /
+                    (ST2_opt - Prev_State.ST1);
+      end if;
+      
+      -- Clamp SC1_opt between 7.5 and 25.
+      if SC1_opt < 7.5 then
+         SC1_opt := 7.5;
+      elsif SC1_opt > 25.0 then
+         SC1_opt := 25.0;
+      end if;
+      
+      -- Calculate optimal SC2
+      declare
+         A : Float := 0.135 + 0.003 * Prev_State.ST2 - 0.0203 * Sensor.ST3;
+         Denom : Float := -0.001 + 0.00004 * Prev_State.ST2;
+      begin
+         if Denom = 0.0 then
+            SC2_opt := 425.0;  -- fallback default
+         else
+            SC2_opt := (((SD1_opt / 24.0) - A) / Denom);
+         end if;
+      end;
+      
+      -- Clamp SC2_opt between 150 and 620.
+      if SC2_opt < 150.0 then
+         SC2_opt := 150.0;
+      elsif SC2_opt > 620.0 then
+         SC2_opt := 620.0;
+      end if;
+      
+      -- Equation 1
+      New_State.ST2 := Prev_State.ST1 + (c / (SC1_opt * Cp * Rho)) * (Beta * Leq * Sensor.SR1 - H * (Tt - Sensor.ST4));
+      
+      -- Equation 3
+      New_State.ST1 := New_State.ST2 - 10.0;
+      
+      -- Equation 4
+      New_State.SD1 := 24.0 * (0.135 + 0.003 * Prev_State.ST2 - 0.0203 * Sensor.ST3 - 0.001 * SC2_opt +
+                               0.00004 * Prev_State.ST2 * SC2_opt);
+   end Calculate_Optimal_State;
 end Calculate_Optimal_Control;
