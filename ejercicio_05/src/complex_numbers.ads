06540b6c80e96f0430fce78832e1b9734d42b04a
grochochotowski <g.m.rochowski@gmail.com>
2025-03-23 21:07:54 +0100

    Define complex type

---
+++ ejercicio_05/src/complex_numbers.ads
@@ -0,0 +1,9 @@
+package Complex_Numbers is
+   
+   -- Define the Complex Number type (real and imaginary)
+   type Complex is record
+      Re : Float;
+      Im : Float;
+   end record;
+
+end Complex_Numbers;
