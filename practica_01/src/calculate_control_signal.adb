0755d14f742ad5e9baef641e6592cd033ddd90e3
grochochotowski <g.m.rochowski@gmail.com>
2025-03-23 14:59:53 +0100

    Define cosntant values

---
+++ practica_01/src/calculate_control_signal.adb
@@ -0,0 +1,13 @@
+with Ada.Text_IO; use Ada.Text_IO;
+package body Calculate_Control_Signal is
+
+   -- Define constants:
+   Beta : constant Float := 0.13;
+   Leq : constant Float := 16.0;
+   H : constant Float := 4.0;
+   c : constant Float := 9.0 * 2.0 * 6.0 * 10000.0;
+   Cp : constant Float := 4190.0;
+   Rho : constant Float := 976.0;
+
+   
+end Calculate_Control_Signal;
