9daa61673b5629339a25705d651cf145b95d6ad2
grochochotowski <g.m.rochowski@gmail.com>
2025-03-23 15:00:16 +0100

    Initialize caluclations by setting beggining stat

--- practica_01/src/calculate_control_signal.adb
+++ practica_01/src/calculate_control_signal.adb
@@ -9,5 +9,13 @@ package body Calculate_Control_Signal is
    Cp : constant Float := 4190.0;
    Rho : constant Float := 976.0;
 
+   -- Initialize procedure
+   procedure Initialize(State : out Simulation_State) is
+   begin
+      State.ST1 := 48.0;
+      State.ST2 := 58.0;
+      State.SD1 := 0.0;
+   end Initialize;
+
    
 end Calculate_Control_Signal;
