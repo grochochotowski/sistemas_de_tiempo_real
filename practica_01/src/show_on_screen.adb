69c380e7361f63463721cd982b617027e81f6dd2
grochochotowski <g.m.rochowski@gmail.com>
2025-03-23 16:31:09 +0100

    Make screen logging more readable

--- practica_01/src/show_on_screen.adb
+++ practica_01/src/show_on_screen.adb
@@ -5,15 +5,20 @@ with All_Types; use All_Types;
 package body Show_On_Screen is
    procedure Display_State(State : in Control_State; Sensor : in Sensor_Data) is
    begin
-      Put("ST1: ");
-      Ada.Float_Text_IO.Put(State.ST1, Fore => 6, Aft => 2, Exp => 0);
+      Put("State ->  ST1: ");
+      Ada.Float_Text_IO.Put(State.ST1, Fore => 3, Aft => 2, Exp => 0);
       Put("  ST2: ");
-      Ada.Float_Text_IO.Put(State.ST2, Fore => 6, Aft => 2, Exp => 0);
+      Ada.Float_Text_IO.Put(State.ST2, Fore => 3, Aft => 2, Exp => 0);
       Put("  SD1: ");
-      Ada.Float_Text_IO.Put(State.SD1, Fore => 6, Aft => 2, Exp => 0);
+      Ada.Float_Text_IO.Put(State.SD1, Fore => 3, Aft => 2, Exp => 0);
       New_Line;
-      Put_Line("Sensor -> SR1: " & Float'Image(Sensor.SR1) &
-               "  ST4: " & Float'Image(Sensor.ST4) &
-               "  ST3: " & Float'Image(Sensor.ST3));
+      Put("Sensor -> SR1: ");
+      Ada.Float_Text_IO.Put(Sensor.SR1, Fore => 3, Aft => 2, Exp => 0);
+      Put("  ST4: ");
+      Ada.Float_Text_IO.Put(Sensor.ST4, Fore => 3, Aft => 2, Exp => 0);
+      Put("  SD3: ");
+      Ada.Float_Text_IO.Put(Sensor.ST3, Fore => 3, Aft => 2, Exp => 0);
+      New_Line;
+      New_Line;
    end Display_State;
 end Show_On_Screen;
