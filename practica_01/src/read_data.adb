df1ee123d800274c53c0768cde02877b7ca35c74
grochochotowski <g.m.rochowski@gmail.com>
2025-03-23 14:23:52 +0100

    Update read_data body for working with 2d table

--- practica_01/src/read_data.adb
+++ practica_01/src/read_data.adb
@@ -1,18 +1,28 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Float_Text_IO; use Ada.Float_Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Read_Data;
 
 package body Read_Data is
     procedure Read_Text (File_Name :  in String; Data : out Array_Data) is
       File : File_Type;
-      Value : Float;
+      Dummy_Index : Integer;
+      Value1, Value2, Value3 : Float;
       Index : Integer := 1;
+
    begin
       Open (File, In_File, File_Name);
 
-      while not End_Of_File(File) and then Index <= 9 loop
-         Get (File, Value);
-         Data(Index) := Value;
+      while not End_Of_File(File) and then Index <= Data'Last loop
+         Get (File, Dummy_Index);
+         Get (File, Value1);
+         Get (File, Value2);
+         Get (File, Value3);
+
+         Data(Index)(1) := Value1;
+         Data(Index)(2) := Value2;
+         Data(Index)(3) := Value3;
+
          Index := Index + 1;
       end loop;
 
