74ccbcd263940d72f41361013abdcbe639cb3a3c
grochochotowski <g.m.rochowski@gmail.com>
2025-03-23 14:18:19 +0100

    Fix implementation of procedure to work with 2d arrays

--- practica_01/src/read_data.ads
+++ practica_01/src/read_data.ads
@@ -1,4 +1,6 @@
 package Read_Data is
-   type Array_Data is array (1 .. 15) of Float;
+   type Row_Type is array (1 .. 3) of Float;
+   type Array_Data is array (1 .. 20) of Row_Type;
+
    procedure Read_Text (File_Name:  in String; Data : out Array_Data);
 end Read_Data;
