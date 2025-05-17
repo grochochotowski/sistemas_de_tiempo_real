d58b273c2cfebd4ff4b817e1f7640083dac2e2cb
grochochotowski <g.m.rochowski@gmail.com>
2025-05-17 20:38:22 +0200

    Write main logic behind server communication

--- ejercicio_10/src/main.adb
+++ ejercicio_10/src/main.adb
@@ -1,6 +1,19 @@
+with PutterServer; use PutterServer;
+
 procedure Main is
+   task type PutterClient(Name : Character);
+
+   task body PutterClient is
+   begin
+      for I in 1 .. 5 loop
+         Put_Line("Task " & Name & ": " & I'Image); -- Calls PutterServer.Put_Line
+      end loop;
+   end PutterClient;
+
+   A : PutterClient('A');
+   B : PutterClient('B');
+   C : PutterClient('C');
 
 begin
-   --  Insert code here.
    null;
 end Main;
