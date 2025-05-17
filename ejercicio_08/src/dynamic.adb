383e6c0fb5b60127f10be35ab9b0f3bf90e8bd48
grochochotowski <g.m.rochowski@gmail.com>
2025-05-17 17:19:38 +0200

    Create base for dynamic version

--- ejercicio_08/src/dynamic.adb
+++ ejercicio_08/src/dynamic.adb
@@ -1,4 +1,11 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
 procedure dynamic is
-begin
    
+   
+   
+begin
+   A := new Task_Type('A');
+   B := new Task_Type('B');
+   C := new Task_Type('C');
 end dynamic;
