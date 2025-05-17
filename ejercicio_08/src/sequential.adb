4fdc961315d35faaef65aa3daf1977b11eaacd3d
grochochotowski <g.m.rochowski@gmail.com>
2025-05-17 16:59:11 +0200

    Write task A

--- ejercicio_08/src/sequential.adb
+++ ejercicio_08/src/sequential.adb
@@ -1,5 +1,12 @@
 procedure sequential is
 
+   procedure Task_A(I : Positive) is
+   begin
+      Put_Line("Task A: " & I'Image);
+   end Task_A;
+
+
+
 begin
    for I in 1 .. 5 loop
       Task_A(I);
