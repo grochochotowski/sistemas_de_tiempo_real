49afc1bbf8a9bfff2add0c5f547b97324c036039
grochochotowski <g.m.rochowski@gmail.com>
2025-05-31 14:04:47 +0200

    Create shared resource X

--- ejercicio_12/src/main.adb
+++ ejercicio_12/src/main.adb
@@ -1,5 +1,17 @@
 procedure Main is
 
+   -- Shared Resource X
+   protected Resource_X is
+      procedure Use(Time_In_Use : Time_Span);
+   end Resource_X;
+
+   protected body Resource_X is
+      procedure Use(Time_In_Use : Time_Span) is
+      begin
+         delay Time_In_Use;
+      end Use;
+   end Resource_X;
+
 begin
 
    null;
