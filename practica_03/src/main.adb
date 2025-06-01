35b834cc4d55d96acc08fa0dc53ad0ce57a5501a
grochochotowski <g.m.rochowski@gmail.com>
2025-06-01 17:33:17 +0200

    Add needed packages and add needed variables

--- practica_03/src/main.adb
+++ practica_03/src/main.adb
@@ -1,3 +1,15 @@
+with Ada.Text_IO;
+with Ada.Real_Time;
+with System;
+
+use Ada.Text_IO;
+use Ada.Real_Time;
+use System;
+
+Period_Safety  : constant Time_Span := Milliseconds(25);
+Period_Solar   : constant Time_Span := Milliseconds(50);
+Period_MD      : constant Time_Span := Milliseconds(100);
+
 procedure Main is
 
 begin
