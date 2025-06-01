0d1587347dd1673252d3b12da3f24fe7cf8558bf
grochochotowski <g.m.rochowski@gmail.com>
2025-06-01 17:34:13 +0200

    Add Data_Storage shared resource

--- practica_03/src/main.adb
+++ practica_03/src/main.adb
@@ -10,6 +10,19 @@ Period_Safety  : constant Time_Span := Milliseconds(25);
 Period_Solar   : constant Time_Span := Milliseconds(50);
 Period_MD      : constant Time_Span := Milliseconds(100);
 
+-------------- Shared resources
+protected Data_Storage is
+   procedure Store(Data : String);
+end Data_Storage;
+
+protected body Data_Storage is
+   procedure Store(Data : String) is
+   begin
+      Put_Line("Storing data: " & Data);
+   end Store;
+end Data_Storage;
+
+
 procedure Main is
 
 begin
